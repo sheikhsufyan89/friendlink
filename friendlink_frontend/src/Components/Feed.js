1b6b7d0f7276a73bca97c8669723f09d2cce7828
sheikhsufyan89 <sheikhsufyan89@yahoo.com>
2023-06-19 05:12:47 +0500

    Sidebar component complete
    Main screen navigations complete. Sidebar component complete, with styling and search bar to be visible when a user is logged in. Issues still to resolve include sanity uncertain error.

---
+++ friendlink_frontend/src/Components/Feed.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Client } from "../Client";
+
+const Feed = () => {
+  return <div>Feed</div>;
+};
+
+export default Feed;
